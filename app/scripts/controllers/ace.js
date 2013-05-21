dbfdd1ff698eb030750103db45a75f8c6d904db1
Stanley Gu <stanleygu@gmail.com>
2013-05-21 21:08:01 +0000

    added a place to view a diff

--- app/scripts/controllers/ace.js
+++ app/scripts/controllers/ace.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('yoNodesApp')
-  .controller('AceCtrl', function($scope) {
+  .controller('AceCtrl', function($scope, $http) {
   $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -16,11 +16,25 @@ angular.module('yoNodesApp')
   };
 
   $scope.models = [];
-  $scope.addVersion = function () {
-      var index = $scope.models.length;
-      $scope.models.push({name: index, text: $scope.editorText})
-  }
-  $scope.loadVersion = function (index) {
+  $scope.addVersion = function() {
+    var index = $scope.models.length;
+    $scope.models.push({
+      name: index,
+      text: $scope.editorText,
+      checked: false
+    });
+  };
+  $scope.loadVersion = function(index) {
     $scope.editorText = $scope.models[index].text;
+  };
+
+  $scope.compareVersions = function() {
+    $http.post('http://localhost:3000/bives', {
+      'foo': 'bar'
+    }).success(function() {
+      console.log('Sent request!');
+    }).error(function() {
+      console.log('Error!');
+    });
   }
 });
