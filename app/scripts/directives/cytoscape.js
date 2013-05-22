44b74fb1191741d21668c8a7e1f8be20c00a839c
Stanley Gu <stanleygu@gmail.com>
2013-05-22 20:39:49 +0000

    Added budhat code

--- app/scripts/directives/cytoscape.js
+++ app/scripts/directives/cytoscape.js
@@ -1,12 +1,170 @@
 'use strict';
 
 angular.module('yoNodesApp')
-  .directive('cytoscape', function () {
-    return {
-      template: '<div></div>',
-      restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        element.text('this is the cytoscape directive');
+  .directive('cytoscape', function() {
+  return {
+    template: '<div></div>',
+    restrict: 'E',
+    link: function postLink(scope, element, attrs) {
+      var shapeMapper = {
+        attrName: "node set",
+        entries: [{
+            attrValue: "species",
+            value: "ELLIPSE"
+          }, {
+            attrValue: "reaction",
+            value: "DIAMOND"
+          }
+        ]
+      };
+
+      var visual_style = {
+        global: {
+          backgroundColor: "#ffffff"
+        },
+        nodes: {
+          shape: {
+            defaultValue: "ELLIPSE",
+            discreteMapper: shapeMapper
+          },
+          borderWidth: 1,
+          borderColor: "#000000",
+          size: 25,
+          color: {
+            defaultValue: "#ffffff",
+            discreteMapper: {
+              attrName: "surce",
+              entries: [{
+                  attrValue: 0,
+                  value: "#ffffff"
+                }, {
+                  attrValue: -1,
+                  value: "#ff0000"
+                }, {
+                  attrValue: 1,
+                  value: "#0000ff"
+                }, {
+                  attrValue: 2,
+                  value: "#ffff00"
+                }
+              ]
+            }
+          },
+          labelHorizontalAnchor: "center",
+          label: {
+            passthroughMapper: {
+              attrName: "name"
+            }
+          },
+          tooltipText: "test"
+        },
+        edges: {
+          style: {
+            defaultValue: "SOLID",
+            discreteMapper: {
+              attrName: "modifier",
+              entries: [{
+                  attrValue: "none",
+                  value: "SOLID"
+                }, {
+                  attrValue: "stimulator",
+                  value: "EQUAL_DASH"
+                }, {
+                  attrValue: "inhibitor",
+                  value: "EQUAL_DASH"
+                }, {
+                  attrValue: "unknown",
+                  value: "EQUAL_DASH"
+                }
+              ]
+            }
+
+          },
+          targetArrowShape: {
+            defaultValue: "ARROW",
+            discreteMapper: {
+              attrName: "modifier",
+              entries: [{
+                  attrValue: "none",
+                  value: "ARROW"
+                }, {
+                  attrValue: "stimulator",
+                  value: "ARROW"
+                }, {
+                  attrValue: "inhibitor",
+                  value: "T"
+                }, {
+                  attrValue: "unknown",
+                  value: "CIRCLE"
+                }
+              ]
+            }
+          },
+          width: 3,
+          color: {
+            defaultValue: "#000000",
+            discreteMapper: {
+              attrName: "surce",
+              entries: [{
+                  attrValue: 1,
+                  value: "#000000"
+                }, {
+                  attrValue: -1,
+                  value: "#ff0000"
+                }, {
+                  attrValue: 1,
+                  value: "#0000ff"
+                }, {
+                  attrValue: 2,
+                  value: "#ffff00"
+                }
+              ]
+            }
+          }
+        }
+      };
+
+      var options = {
+        swfPath: "swf/CytoscapeWeb",
+        flashInstallerPath: "swf/playerProductInstall"
+      };
+
+
+      function drawDiffFlash(graph) {
+        var draw_options = {
+          network: graph,
+          edgeLabelsVisible: true,
+          layout: "ForceDirected",
+          visualStyle: visual_style,
+        };
+
+        var diffFlash = new org.cytoscapeweb.Visualization("graphdiffflash", options);
+        diffFlash.draw(draw_options);
       }
-    };
-  });
+
+      function drawTreeFlash(graph) {
+        var draw_options = {
+          network: graph,
+          edgeLabelsVisible: true,
+          layout: "ForceDirected",
+          visualStyle: visual_style,
+        };
+
+        var treeFlash = new org.cytoscapeweb.Visualization("graphtreeflash", options);
+        treeFlash.draw(draw_options);
+      }
+
+      function drawModelVizFlash(graph) {
+        var draw_options = {
+          network: graph,
+          edgeLabelsVisible: true,
+          layout: "ForceDirected",
+          visualStyle: visual_style,
+        };
+
+        var treeFlash = new org.cytoscapeweb.Visualization("graphmodelvizflash", options);
+        treeFlash.draw(draw_options);
+      }
+    }
+  };
+});
